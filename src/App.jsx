785b568c99a87beb12ec2ba4c3d392d39d378ee9
Chris Bongers <chrisbongers@gmail.com>
2022-08-10 17:39:31 +0200

    feat: add notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Counter from "./Counter";
-
 export function App() {
+
+    const createNotification = () => {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icons/icon-48.png',
+            title: 'Hi there 👋',
+            message: 'Just a reminder that you rock!',
+            buttons: [
+                { title: 'I know ☺️'}
+            ],
+            priority: 0
+        });
+    }
+
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen bg-indigo-400 text-6xl font-bold text-white">
-            <p>Welcome 👋</p>
-            <br />
-            <Counter />
+        <div className="flex flex-col items-center justify-center w-screen h-auto bg-indigo-400 p-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 text-2xl px-4 rounded" onClick={createNotification}>Surprise me 🎉</button>
         </div>
     );
 }
