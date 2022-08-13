370e96a2956522b3471a5af93d9e520f30a0245f
Chris Bongers <chrisbongers@gmail.com>
2022-08-13 09:12:17 +0200

    feat: install hooks

--- public/background.js
+++ public/background.js
@@ -1,5 +1,35 @@
 'use strict';
 
+chrome.runtime.onInstalled.addListener(details => {
+    chrome.runtime.setUninstallURL('https://daily-dev-tips.com');
+
+    if(details.reason === 'install') {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icons/icon-48.png',
+            title: 'Hi there 👋',
+            message: 'Welcome to the best extensions you ever installed',
+            buttons: [
+                { title: 'Thanks 😅️'}
+            ],
+            priority: 0
+        });
+    }
+
+    if(details.reason === 'update') {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icons/icon-48.png',
+            title: 'Thank you',
+            message: 'For updating this extensions',
+            buttons: [
+                { title: 'Cool'}
+            ],
+            priority: 0
+        });
+    }
+})
+
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === 'motivation_alarm') {
         chrome.notifications.create({
